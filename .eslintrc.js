0e4f2cafab4fb3ffe1a21a066de986daf5ae76f0
muhtasim1 <muhtasim@greentomatomedia.com>
2022-05-21 21:25:52 +0600

    fixed dependency updates 🧀

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['alloy', 'alloy/react', 'alloy/typescript', 'next'],
+  extends: ["alloy", "alloy/react", "alloy/typescript", "next"],
   // use next/core-web-vitals to error on a number of rules
   // that are warnings by default if they affect Core Web Vitals
   // extends: ['next', 'next/core-web-vitals'],
@@ -12,10 +12,11 @@ module.exports = {
   },
   rules: {
     complexity: "off",
+    "react/no-namespace": "off",
     "prefer-promise-reject-errors": "off",
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-require-imports": 0,
-    'import/no-anonymous-default-export': 'off',
+    "import/no-anonymous-default-export": "off",
   },
 };
